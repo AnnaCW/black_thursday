02e94ec8fc710df71f68f6858ea618640d75d379
Jennifer <jennifersoden@hotmail.com>
2016-04-25 13:24:00 -0600

    changed csv parser tests

--- test/csv_parser_test.rb
+++ test/csv_parser_test.rb
@@ -1,5 +1,6 @@
 require_relative 'test_helper'
 require 'minitest/autorun'
+require 'minitest/pride'
 require_relative '../lib/csv_parser'
 
 class CsvParserTest < Minitest::Test
@@ -10,28 +11,24 @@ class CsvParserTest < Minitest::Test
     @parser = CsvParser.new
   end
 
-  def it_instantiates_parser
+  def test_it_instantiates_parser
     assert parser
   end
 
-  def test_it_parses_merchant_csv_into_array
-    assert_equal Array, parser.merchants("./data/merchants.csv").class
+  def test_it_parses_merchant_csv_into_a_hash
+    assert_equal Hash, parser.collect_data( { :merchants => "./data/merchants.csv" } ).class
   end
 
   def test_it_parses_each_merchant_into_hash
-    assert_equal "12334105", parser.merchants("./data/merchants.csv")[0][:id]
-  end
-
-  def test_it_parses_items_csv_into_array
-    assert_equal Array, parser.items("./data/items.csv").class
+    assert_equal "12334105", parser.collect_data( { :merchants => "./data/merchants.csv" } )[:merchants][0][:id]
   end
 
   def test_it_parses_each_item_into_hash_and_finds_id
-    assert_equal "263395237", parser.items("./data/items.csv")[0][:id]
+    assert_equal "263395237", parser.collect_data( { :items => "./data/items.csv" } )[:items][0][:id]
   end
 
   def test_it_parses_each_item_into_hash_and_finds_name
-    assert_equal "510+ RealPush Icon Set", parser.items("./data/items.csv")[0][:name]
+    assert_equal "510+ RealPush Icon Set", parser.collect_data( { :items => "./data/items.csv" } )[:items][0][:name]
   end
 
 end
